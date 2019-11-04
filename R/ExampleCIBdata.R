9578202b6e98aa2a15cde3b98cb468c6029fd073
alastair-JL <Alastair_JL@xtra.co.nz>
2019-11-04 15:43:58 +0100

    Rename Antdata

---
+++ R/ExampleCIBdata.R
@@ -0,0 +1,16 @@
+#' ExampleCIBdata
+#'
+#' A simple example CIB matrix for use in the examples.
+#' ExampleCIBdata considers the descriptors Population, Economy and Education. 
+#' Each state can take the value either Low, Medium or High. 
+#' For the sake of this example we use the impact matrix previously considered by 
+#' Lloyd and Schweizer (2014).
+#' 
+#' @keywords CIB , data
+#' @name ExampleCIBdata
+#' @author Alastair Jamieson Lane. <aja107@@math.ubc.ca>
+#' @examples
+#' data(ExampleCIBdata)
+#' CIBmatrix<-ExampleCIBdata[[1]]
+#' 
+NULL
